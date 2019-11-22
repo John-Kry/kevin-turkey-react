b75139581ee4dd789bb15f48e6fc8d32be0a395f
John Kryspin <johnkryspin@gmail.com>
2019-11-22 16:58:25 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useCallback, useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bulma/css/bulma.css";
+import { PayPalButton } from "react-paypal-button-v2";
+import credentials from "./credentials/paypal.json";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import collage from "./assets/collage.jpg";
+import turkey from "./assets/turkey-header.jpg";
+import Footer from "./footer";
+import emblem from "./assets/emblem.png";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const client = {
+		sandbox: credentials.client_id
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img className="header-img" src={emblem}></img>
+				<span className="header-text">Kevin the Turkey</span>
+			</header>
+			<section class="section">
+				<div class="container">
+					Hello, welcome to Kevin the Turkey's website!
+					<br />
+					If you would like to order the Kevin the Turky book please
+					press the Paypal button at the bottom of the page!
+				</div>
+			</section>
+			<section class="section">
+				<div class="container">
+					<div class="columns">
+						<div class="column">
+							<Carousel>
+								<div>{<img src={collage} />} </div>
+								<div>
+									<img src={turkey} />
+								</div>
+							</Carousel>
+						</div>
+						<div class="column">
+							<div style={{ borderBottom: "1px solid black" }}>
+								This book is a x page childrens book about the
+								Wethersfield turkey. We reccommend reading this
+								with your child in order to celebrate the
+								thanksgiving spirit.
+							</div>
+							<div>
+								Buy our book directly from us using paypal and
+								we will ship you Kevin the Turkey through UPS?.
+								Expect to recieve your books up to two weeks
+								after ordering.
+							</div>
+							<PayPalButton
+								amount="0.01"
+								// shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+								onSuccess={(details, data) => {
+									alert(
+										"Transaction completed by " +
+											details.payer.name.given_name
+									);
+									// OPTIONAL: Call your server to save the transaction
+									// return fetch("/paypal-transaction-complete", {
+									//   method: "post",
+									//   body: JSON.stringify({
+									//     orderID: data.orderID
+									//   })
+									// });
+									// }}
+								}}
+								options={{ clientId: client.sandbox }}
+							/>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Footer = props => {
+	return (
+		<footer class="footer">
+			<div class="content has-text-centered">
+				<p>
+					The Kevin the Turkey book was authored by Damien and Pam,
+					<div> Website created with ❤️ by John</div>
+				</p>
+			</div>
+		</footer>
+	);
+};
+export default Footer;
